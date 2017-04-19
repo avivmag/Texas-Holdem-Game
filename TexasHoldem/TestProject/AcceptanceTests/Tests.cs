aa3b545211341c2e9d03a4b8d0b8dad1934aab2d
shidlhad <shidlhad@post.bgu.ac.il>
2017-04-19 19:29:54 +0300

    add tests support game

--- TexasHoldem/TestProject/AcceptanceTests/Tests.cs
+++ TexasHoldem/TestProject/AcceptanceTests/Tests.cs
@@ -227,85 +227,7 @@ namespace TestProject
             Assert.AreNotEqual(this.joinLeaguePerPoints(points), null);
         }
 
-        [TestMethod]
-        public void TestBet()
-        {
-            //bet in an existing game
-            Assert.IsFalse(this.isGameOver(game, username));
-            Assert.IsTrue(this.spectateActiveGame(game));
-            //enough chips to bet
-            Assert.IsTrue(this.bet(amountToBet));
-            //bet again in the current round
-            Assert.IsFalse(this.bet(amountToBet));
-            Assert.IsTrue(this.updatePot(amountToBet));
-            Assert.IsTrue(this.updateStatePlayer(statePlayer, amountToBet));
-            //not enough chips
-            Assert.IsFalse(this.updatePot(34326));
-
-        }
-
-        [TestMethod]
-        public void TestRaise()
-        {
-            //raise in an existing game
-            Assert.IsFalse(this.isGameOver(game, username));
-            Assert.IsTrue(this.spectateActiveGame(game));
-            //one player already bet 
-            Assert.IsTrue(this.bet(amountToBet) && this.raise(amountToBet));
-            Assert.IsTrue(this.updatePot(amountToBet));
-            //raise to lower of bet
-            Assert.IsFalse(this.raise(0));
-            //update to raise game
-            Assert.IsTrue(this.updateStatePlayer(statePlayer, amountToBet));
-            //not enough chips
-            Assert.IsFalse(this.updatePot(10000000));
-
-        }
-
-        [TestMethod]
-        public void TestCall()
-        {
-            //call in an existing game
-            Assert.IsFalse(this.isGameOver(game, username));
-            Assert.IsTrue(this.spectateActiveGame(game));
-            //request to call success
-            Assert.IsTrue(this.call(amountToBet));
-            Assert.IsTrue(this.updatePot(amountToBet));
-            Assert.IsTrue(this.updateStatePlayer(statePlayer, amountToBet));
-            //call without enough money
-            Assert.IsFalse(this.call(0));
-            //*NOT SURE IF WE NEED TO IMPLEMENT TEST function FOR ALL-IN*//
-
-        }
-
-        [TestMethod]
-        public void TestFold()
-        {
-            //call in an existing game
-            Assert.IsFalse(this.isGameOver(game, username));
-            Assert.IsTrue(this.spectateActiveGame(game));
-            //fold success
-            Assert.IsTrue(this.fold());
-            // update the amount to be the same as before the fold
-            Assert.IsTrue(this.updateStatePlayer(statePlayer, amountToBet));
-
-        }
-
-        [TestMethod]
-        public void TestCheck()
-        {
-            //call in an existing game
-            Assert.IsFalse(this.isGameOver(game, username));
-            Assert.IsTrue(this.spectateActiveGame(game));
-            //check success
-            Assert.IsTrue(this.check());
-            Assert.IsTrue(this.updatePot(amountToBet));
-            //if one player already bet
-            Assert.IsTrue(this.bet(amountToBet));
-            Assert.IsFalse(this.check());
-
-        }
-
+        
         [TestMethod]
         public void TestBlindBets()
         {
@@ -315,6 +237,7 @@ namespace TestProject
             //check if can blind bet
             Assert.IsTrue(this.betSmallBlind(amountToBet));
             Assert.IsTrue(this.betBigBlind(amountToBet*2));
+            Assert.IsTrue(this.updatePot(amountToBet+ amountToBet*2));
             //not enough to bet
             Assert.IsFalse(this.betBigBlind(0));
             Assert.IsFalse(this.betSmallBlind(0));
