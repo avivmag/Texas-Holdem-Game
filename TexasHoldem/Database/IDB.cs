64d5bf943999ad1a72316f6d546fa98e5ca66065
shidlhad <shidlhad@post.bgu.ac.il>
2017-06-01 20:07:22 +0300

    added DBImpl

---
+++ TexasHoldem/Database/IDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace Database
+{
+    interface IDB
+    {
+
+        DataTable uploadSystemUser();
+        string getEnterMessage(string stringCommand);
+        void editUserName(int userID, string newData);
+
+    }
+}
