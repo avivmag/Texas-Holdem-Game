f14b005c8c339fc590665f1cf28605a3f141091d
Or Papo <orp@mashov.info>
2017-06-07 15:00:45 +0300

    Added statistics.

---
+++ TexasHoldem/WebClient/statistics/statistics.controller.js
@@ -0,0 +1,21 @@
+﻿app.controller("statisticsCtrl", function ($rootScope, $scope, $http) {
+
+    if ($rootScope.login) {
+        $scope.getSystemUsers = $http({
+                method: 'POST',
+                url: 'https://localhost:4343/',
+                data:
+                    {
+                        action: 'GetUsersDetails'
+                    }
+            }).then(function successCallback(response) {
+                $scope.users = response.data;
+            }, function errorCallback(response) {
+                console.log(response);
+                alert("Could not fetch users list.");
+            });
+    }
+    else {
+        alert("Could not fetch data. you are not logged in.");
+    }
+});
