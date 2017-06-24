48f72eff6deefaa327216088ea651cdcd0242bc1
Or Papo <orp@mashov.info>
2017-06-24 15:22:58 +0300

    select replay window.

---
+++ TexasHoldem/PL/SelectReplayWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for SelectReplayWindow.xaml
+    /// </summary>
+    public partial class SelectReplayWindow : Page
+    {
+        public SelectReplayWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void selectGameReplayGrid_Selected(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
